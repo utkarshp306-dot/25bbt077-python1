04d4c5b74dcf32be6975c14a4cc122cc957102e6
UTKARSH PATEL <utkarshp306@gmail.com>
2026-02-02 17:29:51 +0530

    Add files via upload

---
+++ all 1.py
@@ -0,0 +1,12 @@
+def divide():
+    a=int(input("Enter a no:"))
+    b=int(input("Enter a no:"))
+    c=a//b
+    d=a+b
+    e=a-b
+    f=a*b
+    print("division of a and b is",c)
+    print("addition of a and b is",d)
+    print("subtraction of a and b is",e)
+    print("multiplication of a and b is",f)
+divide()

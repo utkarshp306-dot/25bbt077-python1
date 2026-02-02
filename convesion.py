fc44260f2da3d7c453fe3700e95419ecd95c22ca
UTKARSH PATEL <utkarshp306@gmail.com>
2026-02-02 17:41:14 +0530

    Add files via upload

--- convesion.py
+++ convesion.py
@@ -1,8 +1,10 @@
 def convert():
-    a=int(input("enter hours:"))
+   
     b=int(input("enter minutes:"))
-    c=a*60
-    d=c+b
-    print("conversion in minutes is",d)
+    c=b/60
+    
+   
+    print("conversion in hours is",c)
+    
 
 convert()

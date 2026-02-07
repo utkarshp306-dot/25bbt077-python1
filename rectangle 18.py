b579ea0905d15bce5f8bb4913d44c67eb3e92e3e
UTKARSH PATEL <utkarshp306@gmail.com>
2026-02-07 13:07:50 +0530

    Add files via upload

---
+++ rectangle 18.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  7 13:04:18 2026
+
+@author: Admin
+"""
+
+def areaperimetr():
+    a= float(input("enter length of rectangle:"))
+    b= float(input("enter breadth of rectangle:"))
+    c=a*b
+    d=(a+b)*2
+    print('area:',c)
+    print('perimeter:',d)
+    
+areaperimetr()
+    

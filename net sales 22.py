9494a9dfef2fc50a6c47b5c91fed9f149be46681
UTKARSH PATEL <utkarshp306@gmail.com>
2026-02-07 13:25:42 +0530

    Add files via upload

---
+++ net sales 22.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  7 13:20:58 2026
+
+@author: Admin
+"""
+
+def netsales():
+    a= float(input("enter gross sales:"))
+    b=(10*a)/100
+    c=a-b
+    print('net sales :',c)
+    
+netsales()
+    

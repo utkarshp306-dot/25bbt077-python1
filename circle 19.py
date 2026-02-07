89029b3b94c64fa4bdf45030acfd7332bd00a953
UTKARSH PATEL <utkarshp306@gmail.com>
2026-02-07 13:11:11 +0530

    Add files via upload

---
+++ circle 19.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  7 13:07:56 2026
+
+@author: Admin
+"""
+
+def area():
+    a= float(input("enter circle radius:"))
+    c=(22/7)*a*a
+    print('area:',c)
+    
+    
+area()
+    
